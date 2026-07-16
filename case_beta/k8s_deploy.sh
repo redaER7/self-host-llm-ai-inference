af2c9ef65f999877c54566622fba52d067543566
redaer7 <reda.alhor@consensiohealth.com>
2026-07-16 18:10:32 +0200

    Update envoy ai gateway k8s deploys

--- case_beta/k8s_deploy.sh
+++ case_beta/k8s_deploy.sh
@@ -9,10 +9,10 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/main/scrip
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-#set up cluster access
-sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
-sudo chown $USER:$USER ~/.kube/config
-chmod 600 ~/.kube/config
+#set up cluster access on gpu node
+#sudo cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+#sudo chown $USER:$USER ~/.kube/config
+#chmod 600 ~/.kube/config
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
